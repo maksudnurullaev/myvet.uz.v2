6078a9d3e6a7490cf683c5393784adb4b6d547cd
maksudnurullaev <maksud.nurullaev@gmail.com>
2022-11-11 13:07:06 +0500

    Fix last!

--- src/components/HomePageAction.vue
+++ src/components/HomePageAction.vue
@@ -5,7 +5,7 @@
   <div class="card text-center mb-3">
     <div class="card-body">
       <h5 class="card-title">
-        Клиника работает с 09:00 до 18:00 по предварительной записи на прием!
+        Клиника работает с 09:00 до 18:00 по предварительной записи!
       </h5>
       <a href="tel:+998909653034" class="btn btn-primary btn-lg"
         >Позвоните на номер: (90) 965 30 34</a
