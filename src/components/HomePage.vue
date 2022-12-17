a70156a6fd0e7b354d6cf39fe80be3cb07c4ebdd
Maksud Nurullaev <maksud.nurullaev@gmail.uz>
2022-12-17 23:36:08 +0500

    Add default home page contents

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,79 +1,27 @@
 <template>
   <!-- view-Home-->
   <div class="container">
-    <home-page-action />
-    <!-- Цитаты -->
-    <!-- <div class="mt-2 mb-2">
-      <div class="card text-dark bg-light">
-        <template v-for="tt in getNextThought()">
-          <div class="card-body">
-            <img src="/photos/Logo_horizontal_small.jpg" class="rounded float-start me-2 d-none d-lg-block" />
-            <h5 class="card-title">Мысли о животных</h5>
-            <p class="card-text">
-              <span class="font-italic">
-                {{ tt.text }}
-              </span>
-            </p>
-            <a href="{{ tt.authorLink }}"
-              target="_blank" class="card-link">{{ tt.author}}</a>
-            <br /><br />
-            <a href="tel:+998994425080" type="button" class="btn btn-primary bt-3 btn-lg" id="liveAlertBtn">
-              Звоните на номер: (99) 442 50 80
-            </a>
-          </div>
-        </template>
-      </div>
-    </div> -->
-    <!-- <div class="container"> -->
+    <template v-if="isDate(2022, 12, 21)">
+      <home-page-action />
+    </template>
+    <template v-else>
+      <home-page-default />
+    </template>
+
     <home-page-carousel />
-    <!-- </div> -->
   </div>
 </template>
 
 <script setup lang="ts">
 import HomePageCarousel from "@/components/HomePageCarousel.vue";
 import HomePageAction from "@/components/HomePageAction.vue";
+import HomePageDefault from "@/components/HomePageDefault.vue";
 
-interface thought {
-  text: string;
-  author: string;
-  authorLink?: string;
-}
-
-const thoughtsOfTheGreats: thought[][] = [
-  [
-    {
-      text: "Животные суть не что иное, как прообразы наших добродетелей и пороков, блуждающих пред нашим взором, призраки наших душ...",
-      author: "© В. Гюго",
-      authorLink: "https://ru.wikipedia.org/wiki/%D0%93%D1%8E%D0%B3%D0%BE,_%D0%92%D0%B8%D0%BA%D1%82%D0%BE%D1%80",
-    },
-  ],
-  [
-    {
-      text: "Моральный прогресс нации можно измерить тем, как эта нация относится к животным.",
-      author: "© Махатма Ганди",
-      authorLink: "https://citaty.su/zhivotnye-citaty-i-aforizmy-o-zhivotnyx",
-    },
-  ],
-  [
-    {
-      text: "Следует спрашивать не о том, могут ли животные мыслить или говорить, а о том, могут ли они страдать.",
-      author: "© Иеремия Бентам",
-      authorLink: "#https://citaty.su/zhivotnye-citaty-i-aforizmy-o-zhivotnyx",
-    },
-  ],
-  [
-    {
-      text: "Звери — не братья и не слуги. Это другие расы, вместе с нами пойманные в сеть земного существования.",
-      author: "© Генри Бестон",
-      authorLink: "https://citaty.su/zhivotnye-citaty-i-aforizmy-o-zhivotnyx",
-    },
-  ],
-];
-
-// Иеремия Бентам
-
-function getNextThought(): thought[] {
-  return thoughtsOfTheGreats[Math.floor(Math.random()*thoughtsOfTheGreats.length)];
+function isDate(year: number, month: number, day: number): boolean {
+  const d = new Date(),
+    cMonth = "" + (d.getMonth() + 1),
+    cDay = "" + d.getDate(),
+    cYear = d.getFullYear();
+    return year <= cYear && month <= cMonth && day >= cDay;
 }
 </script>
---
+++ src/components/HomePageDefault.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="mt-2 mb-2">
+    <div class="card text-dark bg-light">
+      <template v-for="tt in getNextThought()">
+        <div class="card-body">
+          <img src="/photos/Logo_horizontal_small.jpg" class="rounded float-start me-2 d-none d-lg-block" />
+          <h5 class="card-title">Мысли о животных</h5>
+          <p class="card-text">
+            <span class="font-italic">
+              {{ tt.text }}
+            </span>
+          </p>
+          <a href="{{ tt.authorLink }}" target="_blank" class="card-link">{{ tt.author }}</a>
+          <br /><br />
+          <a href="tel:+998994425080" type="button" class="btn btn-primary bt-3 btn-lg" id="liveAlertBtn">
+            Звоните на номер: (99) 442 50 80
+          </a>
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface thought {
+  text: string;
+  author: string;
+  authorLink?: string;
+}
+
+const thoughtsOfTheGreats: thought[][] = [
+  [
+    {
+      text: "Животные суть не что иное, как прообразы наших добродетелей и пороков, блуждающих пред нашим взором, призраки наших душ...",
+      author: "© В. Гюго",
+      authorLink:
+        "https://ru.wikipedia.org/wiki/%D0%93%D1%8E%D0%B3%D0%BE,_%D0%92%D0%B8%D0%BA%D1%82%D0%BE%D1%80",
+    },
+  ],
+  [
+    {
+      text: "Моральный прогресс нации можно измерить тем, как эта нация относится к животным.",
+      author: "© Махатма Ганди",
+      authorLink: "https://citaty.su/zhivotnye-citaty-i-aforizmy-o-zhivotnyx",
+    },
+  ],
+  [
+    {
+      text: "Следует спрашивать не о том, могут ли животные мыслить или говорить, а о том, могут ли они страдать.",
+      author: "© Иеремия Бентам",
+      authorLink: "#https://citaty.su/zhivotnye-citaty-i-aforizmy-o-zhivotnyx",
+    },
+  ],
+  [
+    {
+      text: "Звери — не братья и не слуги. Это другие расы, вместе с нами пойманные в сеть земного существования.",
+      author: "© Генри Бестон",
+      authorLink: "https://citaty.su/zhivotnye-citaty-i-aforizmy-o-zhivotnyx",
+    },
+  ],
+];
+
+function getNextThought(): thought[] {
+  return thoughtsOfTheGreats[
+    Math.floor(Math.random() * thoughtsOfTheGreats.length)
+  ];
+}
+</script>
